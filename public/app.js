8ef5d01e251134b1aa1b77f7568ad0efe081d719
wooostal <96207003+wooostal@users.noreply.github.com>
2024-09-15 00:19:52 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const name = nameInput.value.trim();
 
         if (amount && name) {
-            const qrCodeData = `Amount: ${amount} CZK\nName: ${name}`;
+            const qrCodeData = `SPD*1.0*CC:CZK*ACC:CZ9140000000008699791200*AM:${amount}*`;
             qrCodeContainer.innerHTML = '';
             new QRCode(qrCodeContainer, {
                 text: qrCodeData,
